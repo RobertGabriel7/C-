9b649de393fa48ca10212aa9bf27810342d844bb
Robert <97358106+RobertGabriel7@users.noreply.github.com>
2023-12-21 15:00:58 -0300

    Docs

--- Programa_002/Program.cs
+++ Programa_002/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// usando o módulo do System
+// using System;
+
+
+
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
