e7e0e1041276aa580041a5655be90f48958d1f51
Robert <97358106+RobertGabriel7@users.noreply.github.com>
2023-12-26 21:13:00 -0300

    Docs: arrumando as pastas

---
+++ Programas/Programa_003/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+/* Proprieda é um membro oferece mecanisco flexivel para ler, gravar ou calcular o valor de um campo particular */
